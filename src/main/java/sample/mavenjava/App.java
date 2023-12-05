feb41c3bf55dd90178ba84ef602b8b9610281e77
durgaprasad <prasaddablikar6@email.com>
2023-12-05 20:51:38 +0530

    first commit

---
+++ src/main/java/sample/mavenjava/App.java
@@ -0,0 +1,13 @@
+package sample.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
